55fb7bfe958c17fb75309c8ca300d7dd7f19c314
pafnootiy <ksander-86@mail.ru>
2023-07-06 21:13:10 +0000

    remove nodejs and npm from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Функция для отправки сообщения в Rollbar
+send_to_rollbar() {
+  local ACCESS_TOKEN="$ROLLBAR_TOKEN"
+  local MESSAGE="$1"
+
+  curl -X POST \
+    -H "Content-Type: application/json" \
+    -d "{\"access_token\": \"$ACCESS_TOKEN\", \"data\": {\"body\": {\"message\": {\"body\": \"$MESSAGE\"}}}}" \
+    https://api.rollbar.com/api/1/item/
+}
+
+# Проверка наличия токена доступа Rollbar
+if [ -z "$ROLLBAR_TOKEN" ]; then
+  echo "Error: ROLLBAR_TOKEN is not set. Please set the environment variable."
+  exit 1
+fi
+
 # Обновление кода репозитория
 echo "Updating repository code..."
 cd /opt/star-burger
@@ -7,24 +24,20 @@ git pull origin master
 
 # Установка библиотек Python
 echo "Installing Python libraries..."
-pip install -r requirements.txt
-
+pip install -r requirements.txt --assume-yes
 
 # Пересборка статики Django
 echo "Collecting Django static files..."
-cd /opt/star-burger
 python manage.py collectstatic --noinput
 
 # Накат миграций
 echo "Applying database migrations..."
-python manage.py migrate
+python manage.py migrate --noinput
 
-# Перезапуск сервисов Systemd
-echo "Restarting Systemd services..."
-sudo systemctl restart star-burger.service
+# Отправка сообщения в Rollbar
+echo "Sending deployment message to Rollbar..."
+send_to_rollbar "Deployment completed successfully."
 
-# Уведомление об успешном завершении деплоя
-echo "Deployment completed successfully."
-
-# В случае ошибки, завершение выполнения скрипта
+# В случае ошибки, отправка сообщения в Rollbar и завершение выполнения скрипта
 set -e
+trap 'send_to_rollbar "Deployment failed."' ERR
